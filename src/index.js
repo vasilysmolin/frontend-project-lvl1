922502b901793b21b468459e728b0850ac60b59a
vasily.smolin <vasilysmoolin@gmail.com>
2021-12-14 12:12:26 +0400

    fix !

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function play(description, generateRound) {
       console.log('Correct!');
     } else {
       console.log(`${userAnswer}! is wrong answer ;(. Correct answer was ${answer}`);
-      console.log(`Let's try again, ${name}`);
+      console.log(`Let's try again, ${name}!`);
       return;
     }
   }
